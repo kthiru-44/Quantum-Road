0b7b09cae055728e3daacf00e7e99e84aa0dcff8
Thiru <thirukumaran3057@gmail.com>
2025-09-11 11:10:06 +0530

    modified entanglement.py

--- Qiskit/Quantum mechanics/Entanglement(Bell's Inequality).py
+++ Qiskit/Quantum mechanics/Entanglement(Bell's Inequality).py
@@ -57,7 +57,7 @@ plot_histogram(total_counts)
 plot_histogram(counts_list)
 plt.show()
 
-# 2. Running in Qiskit-Aer Simulator
+# 2. Running in Qiskit-Aer 
 
 sim = AerSimulator()
 sam_sim = BackendSamplerV2(backend=sim)
